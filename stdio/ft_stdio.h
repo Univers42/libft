16d89166efc8b76550039b713fd0ea4feaa5541e
LESdylan <dev.pro.photo@gmail.com>
2025-04-28 00:58:06 +0000

    updated

--- stdio/ft_stdio.h
+++ stdio/ft_stdio.h
@@ -1,3 +1,6 @@
+#ifndef FT_STDIO_H
+#define FT_STDIO_H
+
 void	ft_first_word(char *str);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -5,3 +8,5 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 int		ft_printf(const char *format, ...);
 int		get_next_line(int fd, char **line);
+
+#endif

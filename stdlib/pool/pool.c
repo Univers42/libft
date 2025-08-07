2ca60b83b7c028db295fa8f91f7f139a2119cd17
LESdylan <dev.pro.photo@gmail.com>
2025-08-08 01:58:13 +0200

    pool complaint with norminette

--- stdlib/pool/pool.c
+++ stdlib/pool/pool.c
@@ -6,7 +6,7 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 01:55:00 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/08/08 01:55:00 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/08/08 01:57:55 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,36 +51,43 @@ void	*pool_alloc(t_pool *pool)
 	return (block->data);
 }
 
-/* free */
-void	pool_free(t_pool *pool, void *ptr)
+static int	pool_free_in_chunk(t_pool *pool, t_pool_chunk *chunk, void *ptr)
 {
-	t_pool_chunk	*chunk;
 	char			*start;
 	char			*end;
 	size_t			bsz;
 	t_pool_block	*block;
 
+	bsz = sizeof(t_pool_block) + pool->block_size;
+	start = chunk->memory;
+	end = start + pool->blocks_per_chunk * bsz;
+	if ((char *)ptr >= start && (char *)ptr < end)
+	{
+		block = (t_pool_block *)((char *)ptr - sizeof(t_pool_block));
+		if (block->is_free == false)
+		{
+			block->is_free = true;
+			block->next = chunk->free_list;
+			chunk->free_list = block;
+			chunk->used_blocks -= 1;
+			pool->total_freed += 1;
+		}
+		return (1);
+	}
+	return (0);
+}
+
+void	pool_free(t_pool *pool, void *ptr)
+{
+	t_pool_chunk	*chunk;
+
 	if (pool == NULL || ptr == NULL)
 		return ;
 	chunk = pool->chunks;
-	bsz = sizeof(t_pool_block) + pool->block_size;
 	while (chunk != NULL)
 	{
-		start = chunk->memory;
-		end = start + pool->blocks_per_chunk * bsz;
-		if ((char *)ptr >= start && (char *)ptr < end)
-		{
-			block = (t_pool_block *)((char *)ptr - sizeof(t_pool_block));
-			if (block->is_free == false)
-			{
-				block->is_free = true;
-				block->next = chunk->free_list;
-				chunk->free_list = block;
-				chunk->used_blocks -= 1;
-				pool->total_freed += 1;
-			}
+		if (pool_free_in_chunk(pool, chunk, ptr))
 			return ;
-		}
 		chunk = chunk->next;
 	}
 }
@@ -102,27 +109,4 @@ void	pool_destroy(t_pool *pool)
 		chunk = next;
 	}
 	pool->chunks = NULL;
-}
-		return ;
-	chunk = pool->chunks;
-	while (chunk)
-	{
-		t_pool_chunk *next = chunk->next;
-		free(chunk->memory);
-		free(chunk);
-		chunk = next;
-	}
-	pool->chunks = NULL;
-}
-
-void pool_stats(t_pool *pool)
-{
-	if (!pool)
-		return;
-	
-	printf("Pool Stats:\n");
-	printf("  Block size: %zu bytes\n", pool->block_size);
-	printf("  Total allocated: %zu\n", pool->total_allocated);
-	printf("  Total freed: %zu\n", pool->total_freed);
-	printf("  Active allocations: %zu\n", pool->total_allocated - pool->total_freed);
 }

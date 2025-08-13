cbab7d3009218fc7ccea2956260aab7aa06d6b2d
LESdylan <dev.pro.photo@gmail.com>
2025-08-13 10:41:34 +0200

    add sigsev function to debug segmentation fault

--- debug/ft_debug.h
+++ debug/ft_debug.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_debug.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: syzygy <syzygy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 09:53:18 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/06/15 18:16:44 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/08/13 10:40:43 by syzygy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ void	ft_print_hex(unsigned char *p, unsigned int size, unsigned long addr);
 void	*ft_print_memory(void *addr, unsigned int size);
 void	*ft_print_array(int *arr, int size, char mode);
 void	print_array(int *arr, int size);
+void	ms_install_segv_handler(void);
 #endif

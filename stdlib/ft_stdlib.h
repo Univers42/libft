d77ea98fc030e4a267dfa4967f2e88deb88f4f4a
LESdylan <dev.pro.photo@gmail.com>
2025-08-13 21:21:23 +0200

    update

--- stdlib/ft_stdlib.h
+++ stdlib/ft_stdlib.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_stdlib.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: syzygy <syzygy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 23:32:19 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/08/08 01:59:22 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/08/13 20:07:17 by syzygy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,11 @@ void	*ft_calloc(t_size nmemb, t_size size);
 void	skip_spaces(int *i, int size, char *str);
 void	dbl_free(char **ptr);
 bool	grow_buf(void **ptr, size_t *cap, size_t start);
+bool	ft_realloc(void **ptr, size_t elem_size,
+					size_t old_count, size_t new_count);
+bool	ft_realloc_grow(void **ptr, size_t elem_size,
+						size_t *cap, size_t min_needed);
+bool	ft_realloc_shrink_to(void **ptr, size_t elem_size,
+								size_t *cap, size_t new_cap);
+                                  
 #endif

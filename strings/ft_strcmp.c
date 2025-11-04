25d132453ad4e02f5d0244fb8fef0436b87a53da
LESdylan <dev.pro.photo@gmail.com>
2025-11-04 03:25:03 +0100

    correction of strcmp impl

--- strings/ft_strcmp.c
+++ strings/ft_strcmp.c
@@ -6,18 +6,18 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 10:04:21 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/06/07 01:20:17 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/11/02 16:22:37 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_string.h"
+#include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+int ft_strcmp(const char *s1, const char *s2)
 {
-	const unsigned char	*str1 = (const unsigned char *)s1;
-	const unsigned char	*str2 = (const unsigned char *)s2;
+	size_t i;
 
-	while (*str1 && *str2 && *str1++ == *str2++)
-		;
-	return (*str1 - *str2);
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }

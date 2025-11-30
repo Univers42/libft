16b3826995091b29c655ac7891c7658d600e74d9
LESdylan <dev.pro.photo@gmail.com>
2025-11-30 14:06:51 +0100

    norminette OK

--- strings/helper_super_trim.c
+++ strings/helper_super_trim.c
@@ -6,7 +6,7 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 01:10:32 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/11/30 14:01:59 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/11/30 14:06:40 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include "ft_ctype.h"
 #include <limits.h>
 #include "trim.h"
-
 
 int	cumul_value(t_addr *ptr, t_addr target)
 {

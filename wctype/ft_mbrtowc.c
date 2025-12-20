5d93088ea8c90867bac4cf36dd3e716d9815eee5
LESdylan <dev.pro.photo@gmail.com>
2025-12-20 23:06:08 +0100

    norminette ok

--- wctype/ft_mbrtowc.c
+++ wctype/ft_mbrtowc.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 18:53:13 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/12/20 22:11:16 by marvin           ###   ########.fr       */
+/*   Updated: 2025/12/20 23:05:51 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,53 +51,69 @@ static wchar_t	asm_codepoint(size_t needed, const unsigned char *s)
 	return (wc);
 }
 
+/* New small helpers to keep logic split but identical in behavior */
+static int	is_ascii(unsigned char c)
+{
+	return (c < 0x80);
+}
+
+static size_t	detect_needed(unsigned char c)
+{
+	if ((c & 0xE0) == 0xC0)
+		return (2);
+	else if ((c & 0xF0) == 0xE0)
+		return (3);
+	else if ((c & 0xF8) == 0xF0)
+		return (4);
+	return (0);
+}
+
+static size_t	validate_and_assemble(wchar_t *pwc, const char *s,
+					size_t needed, size_t n)
+{
+	size_t			i;
+	unsigned char	lead;
+
+	if (needed > n)
+		return ((size_t) - 2);
+	lead = (unsigned char)s[0];
+	i = 1;
+	while (i < needed)
+	{
+		if (((unsigned char)s[i] & 0xC0) != 0x80)
+		{
+			if (pwc)
+				*pwc = (wchar_t)lead;
+			return (1);
+		}
+		++i;
+	}
+	if (pwc)
+		*pwc = asm_codepoint(needed, (const unsigned char *)s);
+	return (needed);
+}
+
 size_t	ft_mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps)
 {
 	unsigned char	c;
-	size_t			i;
 	size_t			needed;
 
 	ft_assert(s && n);
 	if (ps)
 		ft_memset(ps, 0, sizeof(mbstate_t));
 	c = (unsigned char)s[0];
-	if (c < 0x80)
+	if (is_ascii(c))
 	{
 		if (pwc)
 			*pwc = (wchar_t)c;
 		return (1);
 	}
-	/* determine sequence length from leading byte; if it's not a valid leading
-	   byte, treat it as a single byte (avoid returning error that may loop callers) */
-	if ((c & 0xE0) == 0xC0)
-		needed = 2;
-	else if ((c & 0xF0) == 0xE0)
-		needed = 3;
-	else if ((c & 0xF8) == 0xF0)
-		needed = 4;
-	else
+	needed = detect_needed(c);
+	if (needed == 0)
 	{
-		/* invalid leading byte: treat as single-byte */
 		if (pwc)
 			*pwc = (wchar_t)c;
 		return (1);
 	}
-	/* truncated sequence: signal incomplete so caller can provide more bytes */
-	if (needed > n)
-		return ((size_t)-2);
-	/* validate continuation bytes; on invalid continuation, consume first byte */
-	for (i = 1; i < needed; ++i)
-	{
-		if (((unsigned char)s[i] & 0xC0) != 0x80)
-		{
-			/* invalid continuation: treat first byte as single-byte */
-			if (pwc)
-				*pwc = (wchar_t)c;
-			return (1);
-		}
-	}
-	/* assemble codepoint */
-	if (pwc)
-		*pwc = asm_codepoint(needed, (const unsigned char *)s);
-	return (needed);
+	return (validate_and_assemble(pwc, s, needed, n));
 }

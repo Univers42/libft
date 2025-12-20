448ceccad47da10b34365de382213c704c9b11f5
LESdylan <dev.pro.photo@gmail.com>
2025-12-20 03:38:13 +0100

    norminette OK

--- ctype/is_spe_char.c
+++ ctype/is_spe_char.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   is_spe_char.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 21:42:45 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/12/04 21:44:30 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/12/20 03:37:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 #include "ft_ctype.h"
 
-bool    is_spechr(int c)
+bool	is_spechr(int c)
 {
-    const char    *spec = ";$'\"<>|&()\n";
+	const char	*spec = ";$'\"<>|&()\n";
 
-    if (ft_strchr(spec, c) || ft_isspace(c))
-        return (true);
-    return (false);
+	if (ft_strchr(spec, c) || ft_isspace(c))
+		return (true);
+	return (false);
 }

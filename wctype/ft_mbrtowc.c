70b659af7a266962e1b793f4cf4a45125f4bdfd1
LESdylan <dev.pro.photo@gmail.com>
2025-12-20 02:50:13 +0100

    norminette ok

--- wctype/ft_mbrtowc.c
+++ wctype/ft_mbrtowc.c
@@ -3,16 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_mbrtowc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 18:53:13 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/12/09 19:07:37 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/12/20 02:50:01 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_wctype.h"
 #include "wchar.h"
 #include "ft_memory.h"
+#include "ft_debug.h"
+#include "ft_stddef.h"
+#include <unistd.h>
 
 /**
  * Converts a UTF-8 multibyte sequence into a single `wchar_t`
@@ -31,55 +34,63 @@
  * only for UTF-8;
  */
 
-size_t ft_mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps)
+static wchar_t	asm_codepoint(size_t needed, const unsigned char *s)
 {
-    unsigned char c;
-    wchar_t wc;
-    size_t needed;
+	wchar_t		wc;
 
-    // Handle NULL input or zero-length
-    if (!s || n == 0)
-        return 0;
+	wc = 0;
+	if (needed == 2)
+		wc = ((s[0] & 0x1F) << 6) | (s[1] & 0x3F);
+	else if (needed == 3)
+		wc = ((s[0] & 0x0F) << 12) | ((s[1] & 0x3F) << 6) | (s[2] & 0x3F);
+	else if (needed == 4)
+		wc = ((s[0] & 0x07) << 18)
+			| ((s[1] & 0x3F) << 12)
+			| ((s[2] & 0x3F) << 6)
+			| (s[3] & 0x3F);
+	return (wc);
+}
 
-    // Initialize state if needed
-    if (ps)
-        ft_memset(ps, 0, sizeof(mbstate_t));
+static int	determine_utf8_seq_len(unsigned char c, size_t *needed, size_t n)
+{
+	if ((c & 0xE0) == 0xC0)
+		*needed = 2;
+	else if ((c & 0xF0) == 0xE0)
+		*needed = 3;
+	else if ((c & 0xF8) == 0xF0)
+		*needed = 4;
+	else
+		return (invalid("invalid_first_byte", -1));
+	if (*needed > n)
+		return (invalid("invalid_second_byte", -2));
+	return (0);
+}
 
-    c = (unsigned char)s[0];
+size_t	ft_mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps)
+{
+	unsigned char	c;
+	size_t			i;
+	size_t			needed;
+	int				st;
 
-    // ASCII (1-byte)
-    if (c < 0x80)
-    {
-        if (pwc)
-            *pwc = (wchar_t)c;
-        return 1;
-    }
-
-    // Determine UTF-8 sequence length
-    if ((c & 0xE0) == 0xC0) needed = 2;      // 110xxxxx
-    else if ((c & 0xF0) == 0xE0) needed = 3; // 1110xxxx
-    else if ((c & 0xF8) == 0xF0) needed = 4; // 11110xxx
-    else
-        return (size_t)-1; // invalid first byte
-
-    if (needed > n)
-        return (size_t)-2; // incomplete sequence
-
-    // Validate continuation bytes
-    for (size_t i = 1; i < needed; i++)
-        if ((s[i] & 0xC0) != 0x80)
-            return (size_t)-1; // invalid continuation
-
-    // Assemble codepoint
-    if (needed == 2)
-        wc = ((s[0] & 0x1F) << 6) | (s[1] & 0x3F);
-    else if (needed == 3)
-        wc = ((s[0] & 0x0F) << 12) | ((s[1] & 0x3F) << 6) | (s[2] & 0x3F);
-    else // needed == 4
-        wc = ((s[0] & 0x07) << 18) | ((s[1] & 0x3F) << 12) | ((s[2] & 0x3F) << 6) | (s[3] & 0x3F);
-
-    if (pwc)
-        *pwc = wc;
-
-    return needed;
+	ft_assert(s && n);
+	if (ps)
+		ft_memset(ps, 0, sizeof(mbstate_t));
+	c = (unsigned char)s[0];
+	if (c < 0x80)
+	{
+		if (pwc)
+			*pwc = (wchar_t)c;
+		return (1);
+	}
+	st = determine_utf8_seq_len(c, &needed, n);
+	if (st < 0)
+		return ((size_t)st);
+	i = 0;
+	while (++i < needed)
+		if (((unsigned char)s[i] & 0xC0) != 0x80)
+			return (SIZE_MAX);
+	if (pwc)
+		*pwc = asm_codepoint(needed, (const unsigned char *)s);
+	return (needed);
 }

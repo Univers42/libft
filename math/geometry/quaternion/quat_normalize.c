9cdd303d9f43b9f87ea9ad749bfb14018d6abd44
LESdylan <dev.pro.photo@gmail.com>
2025-12-20 03:32:28 +0100

    norminette OK

--- math/geometry/quaternion/quat_normalize.c
+++ math/geometry/quaternion/quat_normalize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quat_normalize.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 14:08:50 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/12/05 02:39:47 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/12/20 03:32:21 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 #include <math.h>
 
 /* normalize a 4-float array in-place */
-static void normalize_quat_arr(float q[4])
+static void	normalize_quat_arr(float q[4])
 {
-	int i;
-	float mag;
+	int		i;
+	float	mag;
 
 	mag = sqrtf(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
 	if (mag == 0.0f)
-		return;
+		return ;
 	i = 0;
 	while (i < 4)
 	{
@@ -31,14 +31,13 @@ static void normalize_quat_arr(float q[4])
 }
 
 /* Provide the C-style symbol expected elsewhere (float[4]) */
-void normalize_quat(float q[4])
+void	normalize_quat(float q[4])
 {
 	normalize_quat_arr(q);
 }
 
 /* Public API expected by headers */
-void quat_normalize(t_quat *q)
+void	quat_normalize(t_quat *q)
 {
-	/* treat the t_quat storage as 4 contiguous floats */
 	normalize_quat_arr((float *)q);
 }

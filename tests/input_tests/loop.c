e1120a43055fe3e026058f98f7e4d77700b6fb46
Dylan Lesieur <dlesieur@c2r19s6.42madrid.com>
2025-12-23 01:45:36 +0100

    update

---
+++ tests/input_tests/loop.c
@@ -0,0 +1,17 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(int argc, char const *argv[])
+{
+	int	pid;
+
+	pid = fork();
+	open("infile", O_RDONLY);
+	while (1)
+	{
+		printf("Helloo miniHELL %i\n", pid);
+		sleep(1);
+	}
+	return (0);
+}

19f797a5a843c97a2ec04102146ae65333442cc0
Dylan Lesieur <dlesieur@c2r2s4.42madrid.com>
2025-12-26 19:22:07 +0100

    update error of compilation

--- tests/input_tests/loop.c
+++ tests/input_tests/loop.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int	main(int argc, char const *argv[])
-{
-	int	pid;
+// int	main(int argc, char const *argv[])
+// {
+// 	int	pid;
 
-	pid = fork();
-	open("infile", O_RDONLY);
-	while (1)
-	{
-		printf("Helloo miniHELL %i\n", pid);
-		sleep(1);
-	}
-	return (0);
-}
+// 	pid = fork();
+// 	open("infile", O_RDONLY);
+// 	while (1)
+// 	{
+// 		printf("Helloo miniHELL %i\n", pid);
+// 		sleep(1);
+// 	}
+// 	return (0);
+// }

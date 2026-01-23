66aa6f400f6adbb44454f59af1d00aacc716b773
LESdylan <dev.pro.photo@gmail.com>
2026-01-23 16:43:49 +0100

    norminette dyn_str OK

--- srcs/ds/dyn_string/dyn_string.h
+++ srcs/ds/dyn_string/dyn_string.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dyn_string.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 17:11:14 by marvin            #+#    #+#             */
-/*   Updated: 2025/12/20 17:11:16 by marvin           ###   ########.fr       */
+/*   Updated: 2026/01/23 16:43:35 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,20 +56,21 @@ void		str_null_terminate(void);
  * THREAD SAFE IN CASE OF MORE COMPLEX CASE
  */
 
-int     dyn_str_init(t_dyn_str *s);
-void    dyn_str_free(t_dyn_str *s);
-int		dyn_str_push(t_dyn_str *s, char el);
-int     dyn_str_pushnstr(t_dyn_str *s, const char *str, size_t n);
-int     dyn_str_pushstr(t_dyn_str *s, const char *str);
-char    dyn_str_pop(t_dyn_str *s);
-int     dyn_str_double(t_dyn_str *s);
-int     dyn_str_idx(t_dyn_str *s, size_t idx);
-void    dyn_str_null_terminate(t_dyn_str *s);
-void    dyn_str_clear(t_dyn_str *s);
-int     dyn_str_ends_with_str(t_dyn_str *s, const char *suffix);
-void    dyn_str_replace_buff(t_dyn_str *s, char *new_buff, size_t new_cap);
-bool    dyn_str_check_at(t_dyn_str *s, size_t pos, int (*fn)(int), const char *cmp);
-void	dyn_str_replace_buff(t_dyn_str *s, char *new_buff, size_t new_cap);
-void	dyn_str_append_fd(t_dyn_str *str, int fd);
-void	dyn_str_update_pos(t_dyn_str *v, size_t val);
+int			dyn_str_init(t_dyn_str *s);
+void		dyn_str_free(t_dyn_str *s);
+int			dyn_str_push(t_dyn_str *s, char el);
+int			dyn_str_pushnstr(t_dyn_str *s, const char *str, size_t n);
+int			dyn_str_pushstr(t_dyn_str *s, const char *str);
+char		dyn_str_pop(t_dyn_str *s);
+int			dyn_str_double(t_dyn_str *s);
+int			dyn_str_idx(t_dyn_str *s, size_t idx);
+void		dyn_str_null_terminate(t_dyn_str *s);
+void		dyn_str_clear(t_dyn_str *s);
+int			dyn_str_ends_with_str(t_dyn_str *s, const char *suffix);
+void		dyn_str_replace_buff(t_dyn_str *s, char *new_buff, size_t new_cap);
+bool		dyn_str_check_at(t_dyn_str *s, size_t pos, int (*fn)(int),
+				const char *cmp);
+void		dyn_str_replace_buff(t_dyn_str *s, char *new_buff, size_t new_cap);
+void		dyn_str_append_fd(t_dyn_str *str, int fd);
+void		dyn_str_update_pos(t_dyn_str *v, size_t val);
 #endif
